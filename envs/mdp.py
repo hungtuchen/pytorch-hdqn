db8368d12326809a2541000756a8c73c2d43e982
transedward <transedward@gmail.com>
2017-05-03 15:32:04 +0800

    Initialize stochastic mdp env

---
+++ envs/mdp.py
@@ -0,0 +1,34 @@
+import random
+
+class StochasticMDPEnv:
+
+    def __init__(self):
+        self.visited_six = False
+        self.current_state = 2
+
+    def reset(self):
+        self.visited_six = False
+        self.current_state = 2
+        return self.current_state
+
+    def step(self, action):
+        if self.current_state != 1:
+            # If "right" selected
+            if action == 1:
+                if random.random() < 0.5 and self.current_state < 6:
+                    self.current_state += 1
+                else:
+                    self.current_state -= 1
+            # If "left" selected
+            if action == 0:
+                self.current_state -= 1
+            # If state 6 reached
+            if self.current_state == 6:
+                self.visited_six = True
+        if self.current_state == 1:
+            if self.visited_six:
+                return self.current_state, 1.00, True
+            else:
+                return self.current_state, 1.00/100.00, True
+        else:
+            return self.current_state, 0.0, False
